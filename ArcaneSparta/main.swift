568bba1c4cdc286be0ec97b6c66d9a57cbc02f37
Kauê Lima <kauekdslopes@gmail.com>
2025-11-14 15:15:04 -0300

    Criando enum e atributos dos guerreiros

--- ArcaneSparta/main.swift
+++ ArcaneSparta/main.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-print("Hello, World!")
+enum OrdemArcana {
+    case helio
+    case nyx
+    case khaos
+}
 
+func atributos(ordem: OrdemArcana) -> (vida: Int, danoBase: Int, critChance: Int, filosofia: String, dica: String) {
+    switch ordem {
+        case .helio:
+            return (vida: 28, danoBase: 4, critChance: 10,
+                    filosofia: "Brilho é disciplina.",
+                    dica: "Hélio domina ataques diretos.")
+        case .nyx:
+            return (vida: 24, danoBase: 5, critChance: 25,
+                    filosofia: "A escuridão observa.",
+                    dica: "Nyx explora críticos com maestria.")
+        case .khaos:
+            return (vida: 22, danoBase: 5, critChance: 40,
+                    filosofia: "O mundo nasceu do acaso.",
+                    dica: "Kháos tem Golpe Arcano poderoso.")
+    }
+}
